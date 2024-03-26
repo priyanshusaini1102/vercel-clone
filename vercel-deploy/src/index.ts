6c302704cd6d15de3228a4d5561e218e38187c5a
priyanshusaini1102 <priyanshusaini1102@gmail.com>
2024-03-26 11:20:44 +0530

    add server to vercel dpeloy

--- vercel-deploy/src/index.ts
+++ vercel-deploy/src/index.ts
@@ -1,6 +1,14 @@
 import { createClient, commandOptions } from "redis";
 import { downloadS3Folder } from "./s3Helper";
 import { buildProject, copyFinalDist } from "./utils";
+import express from 'express'
+import dotenv from 'dotenv'
+import cors from 'cors'
+dotenv.config()
+const app = express()
+app.use(cors());
+app.use(express.json());
+const PORT = process.env.PORT
 
 const subscriber = createClient({
     url: 'rediss://red-co0k8fvsc6pc738rek00:WiyC38q1Da5gYnDHubgrjbcLmAbWncVe@singapore-redis.render.com:6379'
@@ -26,3 +34,15 @@ async function main() {
     }
 }
 main();
+
+app.get('/', (req, res) => {
+  res.json({
+    success: true,
+    message: "server is running"
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+  
+})
